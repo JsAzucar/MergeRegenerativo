6f90929e705c1c8188c184636f54418b075c7290
José Azúcar <ismaelazucaroct@gmail.com>
2025-09-26 16:13:52 -0600

    fix:Cambios a Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,9 +9,8 @@ namespace PruebaMerge
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            //Vamos a probar el merge con Visual Studio
-            lblResultado.Text = txtNombre.text + " Prueba merge ";
-            lblResultado.Text = txtNombre.Text + "  Felicidades sin unión";
+            lblResultado.Text = txtNombre.Text + "  Felicidades lhs";
+            //Necesito que los cambios funciones para reflejar el ejemplo
         }
 
         private void Form1_Load(object sender, EventArgs e)
