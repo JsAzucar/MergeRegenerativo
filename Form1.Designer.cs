97b73273377cafabfab13f6cd6c227e1e1a11d77
José Azúcar <ismaelazucaroct@gmail.com>
2025-09-26 15:41:32 -0600

    feat(merge): Pruebas realizando merge

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PruebaMerge
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnGuardar = new Button();
+            txtNombre = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            lblResultado = new Label();
+            SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(322, 142);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(75, 23);
+            btnGuardar.TabIndex = 0;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(309, 97);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(100, 23);
+            txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(309, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(104, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Escriba su nombre";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(610, 232);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Resultado";
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(586, 268);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(27, 15);
+            lblResultado.TabIndex = 4;
+            lblResultado.Text = "lblR";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblResultado);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtNombre);
+            Controls.Add(btnGuardar);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnGuardar;
+        private TextBox txtNombre;
+        private Label label1;
+        private Label label2;
+        private Label lblResultado;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace PruebaMerge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = txtNombre.Text + "  Felicidades lhs";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
