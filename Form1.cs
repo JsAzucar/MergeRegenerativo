64b4aa70fa0ba7b1a777ab3a8ecd1d1e7c9c10d7
JsAzucar <149110689+JsAzucar@users.noreply.github.com>
2025-09-26 15:56:31 -0600

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,9 @@ namespace PruebaMerge
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            lblResultado.Text = txtNombre.Text + "  Felicidades lhs";
+            //Vamos a probar el merge con Visual Studio
+            lblResultado.Text = txtNombre.text + " Prueba merge ";
+            lblResultado.Text = txtNombre.Text + "  Felicidades sin unión";
         }
 
         private void Form1_Load(object sender, EventArgs e)
